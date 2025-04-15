9787e4986a88437671b05737ff86ad5adc03f027
natandaniel <natan.daniel0@gmail.com>
2025-04-15 22:38:05 +0200

    Remove artificial delays in revenue and invoice data fetching functions to improve performance and streamline code.

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -13,12 +13,6 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    console.log("Fetching revenue data...");
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
     console.log("Data fetch completed after 3 seconds.");
@@ -31,9 +25,6 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
-  console.log("Fetching latest invoices...");
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
   try {
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
